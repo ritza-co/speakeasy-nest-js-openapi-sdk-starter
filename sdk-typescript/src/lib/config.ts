c781feb4033e562266aa19fcf47151cbf30ef321
Niel Thiart <niel@thiart.com>
2024-10-29 20:57:59 +0200

    add discriminator to polymorphic responses

--- sdk-typescript/src/lib/config.ts
+++ sdk-typescript/src/lib/config.ts
@@ -55,7 +55,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0",
-  sdkVersion: "0.0.1",
+  sdkVersion: "0.0.2",
   genVersion: "2.442.11",
-  userAgent: "speakeasy-sdk/typescript 0.0.1 2.442.11 1.0 sdk",
+  userAgent: "speakeasy-sdk/typescript 0.0.2 2.442.11 1.0 sdk",
 } as const;
